17101ca70507cb00ebd1204d328f2d56478158d4
Hiron Das <hiron_cse@yahoo.com>
2016-10-19 21:57:04 +0600

    at page 27

--- app.ts
+++ app.ts
@@ -22,4 +22,39 @@ class HelloWorld {
 	}
 }
 
+@Component({
+	selector: 'reddit',
+	template: `
+		<form class="ui large form segment">
+			<h3 class="ui header">Add a Link</h3>
+
+			<div class="field">
+				<label for="title"> Title: </label>
+				<input name = "title" #newtitle>
+			</div>
+			<div class="field">
+				<label for="link"> Link: </label>
+				<input name = "link" #newlink>
+			</div>
+
+			<button (click)="addArticle(newtitle, newlink)"
+				class="ui positive right floated button">
+				Submit link
+			</button>
+		</form>
+	`
+})
+
+class RedditApp {
+	
+	constructor() {
+					
+	}
+
+	addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+		console.log(`Adding article title: ${title.value} and Link: ${link.value}`);
+	}
+}
+
 bootstrap(HelloWorld);
+bootstrap(RedditApp);
