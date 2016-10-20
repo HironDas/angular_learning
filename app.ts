603826bb0f46b9197e495e6b2ba8019166407685
Hiron Das <hiron_cse@yahoo.com>
2016-10-20 21:40:22 +0600

    now at page 34

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
 
-@Component({
+/*@Component({
 	selector: 'hello-world',
 	template: `
 	<ul>
@@ -20,10 +20,65 @@ class HelloWorld {
 	constructor(){
 		this.names = ["Hiron", "Suvash", "Subrata"];
 	}
+}*/
+
+
+
+@Component({
+	selector: 'reddit-article',
+	host: {
+		class: "row"
+	},
+	template: `
+	<div class = "four wide column center aligned votes">
+		<div class = "ui statistic">
+			<div class = "value">
+				{{value}}
+			</div>
+			<div class = "label">
+				Points
+			</div>
+		</div>
+	</div>
+	<div class = "twelve wide column">
+		<a class = "ui large header" href = "{{ link }}">
+			{{title}}
+		</a>
+		<ul class = "ui big horizontal list voters">
+			<li class = "item">
+				<a href (click) = "voteUp()">
+					<i class = "arrow down icon"></i>
+					downvote
+				</a>
+			</li>
+		</ul>
+	</div>
+	`
+})
+
+ class ArticleComponent  {
+	votes: number;
+	title: string;
+	link : string;
+
+	constructor() {
+		this.title = "Angular 2";
+		this.link = "http://angular.io";
+		this.votes = 10;
+	}
+
+	voteUp(){
+		this.votes += 1;
+	}
+
+	voteDown() {
+		this.votes -= 1;
+	}
 }
 
 @Component({
 	selector: 'reddit',
+	directives: [ArticleComponent],
 	template: `
 		<form class="ui large form segment">
 			<h3 class="ui header">Add a Link</h3>
@@ -56,5 +111,6 @@ class RedditApp {
 	}
 }
 
-bootstrap(HelloWorld);
+
+// bootstrap(HelloWorld);
 bootstrap(RedditApp);
